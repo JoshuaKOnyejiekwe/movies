9a386d32f367315d614339cf751b81ef891c4235
Joshua Onyejiekwe <joshuaonyejiekwe12@gmail.com>
2026-06-04 01:02:14 -0400

    Hud, Gui, changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_02_054746) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_03_230653) do
   create_table "movies", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "genre"
@@ -19,5 +19,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_02_054746) do
     t.string "title"
     t.datetime "updated_at", null: false
     t.boolean "watched"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
