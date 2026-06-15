5d303e2dd43b983855b8ba6e8de8b93187315648
Joshua Onyejiekwe <joshuaonyejiekwe12@gmail.com>
2026-06-15 14:36:16 -0400

    establishing the DB, still need works

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 gem "rails", "~> 8.1.3"
 gem "devise"
 gem "httparty"
+gem "dotenv"
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
 gem "propshaft"
 # Use sqlite3 as the database for Active Record
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,12 +8,14 @@ class MoviesController < ApplicationController
 
   # GET /movies/1 or /movies/1.json
   def show
-    
   end
 
   # GET /movies/new
   def new
     @movie = Movie.new
+    @movie.title = params[:title] if params[:title]
+    @movie.review = params[:review] if params[:review]
+    @movie.genre = params[:genre] if params[:genre]
   end
 
   # GET /movies/1/edit
@@ -58,7 +60,29 @@ class MoviesController < ApplicationController
     end
   end
 
+
+
+ # GET /search_tmdb
+def search_tmdb
+  require "net/http"
+  require "json"
+
+  query = params[:query]
+  api_key = ENV["TMDB_API_KEY"]
+  uri = URI("https://api.themoviedb.org/3/search/movie")
+  uri.query = URI.encode_www_form(api_key: api_key, query: query)
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  response = http.get(uri.request_uri)
+  @results = JSON.parse(response.body)["results"]
+
+  render :search_tmdb
+end
+
+
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_movie
       @movie = Movie.find(params.expect(:id))
